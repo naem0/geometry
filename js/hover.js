f68bd70a8cc25825a37ac1274827e5794b3db0da
MD Naem <mmnayeem408@gmail.com>
2023-02-18 22:52:22 +0600

    all card hover add

--- js/hover.js
+++ js/hover.js
@@ -1,9 +1,18 @@
-const myElement = document.getElementById('my-element');
-const colors = ['red', 'blue', 'green', 'purple', 'orange']; 
-let colorIndex = 0; 
 
-myElement.addEventListener('mouseenter', function() {
+function hover(cardname) {
+  const myElement = document.getElementById(cardname);
+  const colors = ['red', 'blue', 'green', 'purple', 'orange']; 
+  let colorIndex = 0; 
 
-  myElement.style.backgroundColor = colors[colorIndex];
-  colorIndex = (colorIndex + 1) % colors.length;
-});
+  myElement.addEventListener('mouseenter', function() {
+
+    myElement.style.backgroundColor = colors[colorIndex];
+    colorIndex = (colorIndex + 1) % colors.length;
+  });
+}
+hover('triangle-card');
+hover('rectangle-card');
+hover('parallelogram-card');
+hover('rhombus-card');
+hover('pentagon-card');
+hover('ellipse-card');
